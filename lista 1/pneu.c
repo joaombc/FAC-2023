ff78e7cfe0d7a1bd5057769f52d6d8c6a625a77c
João Paulo Monteiro de Barros Ceva Rodrigues <65778250+joaombc@users.noreply.github.com>
2023-09-25 19:36:14 -0300

    Add files via upload

---
+++ lista 1/pneu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int n,m;
+
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    int resultado;
+
+    resultado = n-m;
+
+    printf("%d\n", resultado);
+
+}
